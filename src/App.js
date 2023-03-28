342b3a2ea4bacbce487c9e299e9e0cdfc6b45cc1
Your Name <you@example.com>
2023-03-28 12:16:55 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Row from "./Components/Row";
+import requests from "./requests";
+import Banner from "./Components/Banner";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Banner />
+
+      <Row
+        title="NEFLIX TRENDINGS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="NEFLIX ORIGINALS" fetchUrl={requests.fetchTrending} />
+      <Row title="NEFLIX TOP RATED" fetchUrl={requests.fetchTopRated} />
+      <Row title="NEFLIX ACTION MOVIES" fetchUrl={requests.fetchActionMovies} />
+      <Row title="NEFLIX COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="NEFLIX HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies} />
+      <Row
+        title="NEFLIX ROMANCE MOVIES"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row title="NEFLIX DOCUMENTRIES" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
